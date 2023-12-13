490509696deb0fc4cd3c2459a96219a4eb6cab00
tyrnanbatch <tyrnan.batch@gmail.com>
2023-12-13 13:30:14 +0000

    data visualisatiin now takes up 100% of its container

--- src/components/bars/Bars.tsx
+++ src/components/bars/Bars.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Bar from '../bar/Bar'
 
 let data: number[] = [
-    100, 150, 200, 100, 123
+    100, 150, 200, 100, 123, 400, 314, 54, 31, 1, 300
 ]
 
 export default class Bars extends React.Component {
