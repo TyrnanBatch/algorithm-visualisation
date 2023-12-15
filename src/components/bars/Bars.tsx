1769131fbaaf251bb25a75ae3c223983c27a4ab8
tyrnanbatch <tyrnan.batch@gmail.com>
2023-12-15 18:31:29 +0000

    data now changes when updated

--- src/components/bars/Bars.tsx
+++ src/components/bars/Bars.tsx
@@ -1,37 +1,49 @@
-import './Bars.css'
-import React from "react";
-import Bar from '../bar/Bar'
-
-let data: number[] = [
-    0,0,0,100,0,100,0,0,0
-];
+import './Bars.css';
+import React, { useState } from "react";
+import Bar from '../bar/Bar';
 
 export default function Bars(): React.ReactElement {
+    let initialData: number[] = [
+        100, 300, 450, 250, 100, 300, 350, 50, 150, 450
+    ];
 
-    const getGreatestValue = () => {
+    let nextData: number[] = [
+        450, 300, 150, 350, 300, 100, 250, 50, 450, 100
+    ];
+
+    const getGreatestValue = (): number => {
         let greatestValue: number = 0;
-        for (let i: number = 0; i < data.length; i++) {
-            if (data[i] > greatestValue) {
-                greatestValue = data[i];
+        for (let i: number = 0; i < initialData.length; i++) {
+            if (initialData[i] > greatestValue) {
+                greatestValue = initialData[i];
             }
         }
-        console.log(greatestValue)
         return greatestValue;
     };
 
-    let bars: React.ReactElement = (
-        <>
-            {
-                data.map((val: number) => {
-                    return <Bar width={100 / data.length} height={(val/getGreatestValue())*100}/>
-                })
-            }
-        </>
-    );
+    const renderData = () => {
+        return (
+            initialData.map((val: number, index: number) => (
+                <Bar key={index} width={100 / initialData.length} height={(val / getGreatestValue()) * 100} />
+            ))
+        );
+    };
+
+    const [bars, setBars] = useState(renderData);
+
+    const handleButtonClick = () => {
+        console.log(initialData, 3);
+        initialData = nextData
+        setBars(renderData);
+        console.log(initialData, 4);
+    };
 
     return (
-        <div className={"main-container"}>
-            {bars}
-        </div>
+        <>
+            <button onClick={handleButtonClick} style={{ position: "absolute" }}>Update Data</button>
+            <div className={"main-container"}>
+                {bars}
+            </div>
+        </>
     );
 };
